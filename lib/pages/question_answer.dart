e076ec7a756fb218143e102ae1611741fbdb2d83
VinuBalagopalAP <vinubalagopalap@gmail.com>
2021-03-07 12:24:41 +0530

    noon changes

--- lib/pages/question_answer.dart
+++ lib/pages/question_answer.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:trial/models/answer.dart';
 
 class QuestionAnswerPage extends StatefulWidget {
   @override
@@ -8,14 +11,25 @@ class QuestionAnswerPage extends StatefulWidget {
 
 class _QuestionAnswerPageState extends State<QuestionAnswerPage> {
   // Text editing controller for question text field
-  // TextEditingController _questionFieldController = TextEditingController();
+
+  TextEditingController _questionFieldController = TextEditingController();
+
+  /// To store the current answer object
+
+  Answer _currentAnswer;
 
   /// Handle the process of getting ayes/no answer
   _handleGetAnswer() async {
     try {
       http.Response response =
           await http.get(Uri.parse("https://yesno.wtf/api"));
-      print(response.body);
+      if (response.statusCode == 200 && response.body != null) {
+        Map<String, dynamic> responseBody = json.decode(response.body);
+        Answer answer = Answer.fromMap(responseBody);
+        setState(() {
+          _currentAnswer = answer;
+        });
+      }
     } catch (err, stacktrace) {
       print(err);
       print(stacktrace);
@@ -38,19 +52,34 @@ class _QuestionAnswerPageState extends State<QuestionAnswerPage> {
               width: 0.5 * MediaQuery.of(context).size.width,
               child: TextField(
                 decoration: InputDecoration(
-                  labelText: 'Ask a uestion',
+                  labelText: 'Ask a question',
                   border: OutlineInputBorder(),
                 ),
               )),
           SizedBox(
             height: 20,
           ),
+          if (_currentAnswer != null)
+            Container(
+              height: 250,
+              width: 400,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                image: DecorationImage(
+                  image: NetworkImage(_currentAnswer.image),
+                ),
+              ),
+            ),
+          if (_currentAnswer != null)
+            SizedBox(
+              height: 20,
+            ),
           Row(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 ElevatedButton(
-                  onPressed: _handleGetAnswer(),
+                  onPressed: _handleGetAnswer,
                   child: Text("Get Answer"),
                 ),
                 SizedBox(
